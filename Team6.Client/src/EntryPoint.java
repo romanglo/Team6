4fb40db153c1ae2863343c42ccbe86f2f8b1dd79
Roman Glozman <romanglozman92@gmail.com>
2017-11-14 01:20:16 +0200

    Created project skeleton.

---
+++ Team6.Client/src/EntryPoint.java
@@ -0,0 +1,26 @@
+/**
+ * Package Name: 
+ * Created:		 13-11-2017
+ */
+
+/**
+ *
+ * EntryPoint:
+ * The class that have the main function. 
+ * 
+ */
+public class EntryPoint
+{
+
+	/**
+	 * Client application entry point
+	 *
+	 * @param args application arguments
+	 */
+	public static void main(String[] args)
+	{
+		System.out.println("This is 'Client' application.");
+	}
+
+
+}
---
+++ Team6.Server/src/EntryPoint.java
@@ -0,0 +1,27 @@
+
+/**
+ * Package Name: 
+ * Created:		 13-11-2017
+ */
+
+/**
+ *
+ * EntryPoint:
+ * The class that have the main function. 
+ * 
+ */
+public class EntryPoint
+{
+
+	/**
+	 * Server application entry point
+	 *
+	 * @param args application arguments
+	 */
+	public static void main(String[] args)
+	{
+		System.out.println("This is 'Server' application.");
+	}
+
+
+}
