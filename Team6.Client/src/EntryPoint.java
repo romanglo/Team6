fec3d599ca92d642adca500c79dcc7af4f59a442
Yoni Shpund <ShpundYoni@s.braude.ac.il>
2017-12-09 20:18:20 +0200

    1. Add: UI controller. 2. Update: EntryPoint.java with GUI and server connection.
    Signed-off-by: Yoni Shpund <ShpundYoni@s.braude.ac.il>

--- Team6.Client/src/EntryPoint.java
+++ Team6.Client/src/EntryPoint.java
@@ -1,51 +1,99 @@
+import java.io.IOException;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
 import configurations.ClientConfiguration;
 import logs.LogManager;
-
-/**
- * Package Name: Created: 13-11-2017
- */
+import connectivity.Client;
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
 
 /**
  *
  * EntryPoint: The class that have the main function.
  * 
  */
-public class EntryPoint
-{
+public class EntryPoint extends Application {
 
 	private static Logger s_logger = null;
 	private static ClientConfiguration s_clientConfiguration;
+	
+	/**
+	 * Client connection management instance.
+	 */
+	public static Client clientController;
 
 	/**
-	 * Server application entry point
+	 * Client application entry point
 	 *
-	 * @param args
-	 *            application arguments
+	 * @param args - Application arguments
 	 */
-	public static void main(String[] args)
-	{
+	public static void main(String[] args) {
 		s_logger = LogManager.getLogger();
 
 		try {
 			initializeConfiguration();
+			launch(args);
 		}
 		catch (Exception ex) {
-			s_logger.log(Level.SEVERE, "initialization failed!", ex);
+			s_logger.log(Level.SEVERE, "Initialization failed!", ex);
 		}
 
 	}
-
+	
+	/**
+	 * Method closes the connection with the server.
+	 *
+	 */
+	public static void disposeConnection() {
+		if (clientController != null) {
+			clientController.close();
+			s_logger.info("Client disposed successfully");
+		}
+	}
+	
+	/**
+	 * Method creates connection to the server with the configured IP and port.
+	 *
+	 * @throws IOException - Connection error.
+	 * @throws NumberFormatException - Conversion error.
+	 */
+	public static void initializeConnection() throws IOException, NumberFormatException {
+		clientController = new Client(s_logger, s_clientConfiguration.getIp(), 
+				Integer.parseInt(s_clientConfiguration.getPort()));
+		s_logger.info("Client initialized successfully.");
+	}
+	
+	/**
+	 * Method reads from the configuration file and sets the configuration.
+	 *
+	 */
 	private static void initializeConfiguration() {
 		s_clientConfiguration = ClientConfiguration.getInstance();
 		if (s_clientConfiguration.isDefaultConfiguration()) {
 			s_logger.warning("Failed on try to read configuration from \"" + 
-					ClientConfiguration.CONFIGURATION_PATH
-					+ "\". Created default configuration.");
-
+					ClientConfiguration.CONFIGURATION_PATH + "\". Created default configuration.");
 		}
 		s_logger.config("Client configuration loaded:" + s_clientConfiguration.toString());
+	}
+	
+	
+	/**
+	 * Method opens the UI of the client.
+	 */
+	@Override
+	public void start(Stage primaryStage) {
+		try {
+			Parent root = FXMLLoader.load(getClass().getResource("Main.fxml"));
+			Scene scene = new Scene(root);
+			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
+			primaryStage.setScene(scene);
+			primaryStage.show();
+		} catch(Exception e) {
+			e.printStackTrace();
+		}
 	}
 }
