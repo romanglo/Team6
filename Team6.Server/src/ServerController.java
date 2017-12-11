02139c0922dcfbf23402328eb1ba1d779833443d
Shimon Elfassy <shimon.e456@gmail.com>
2017-12-11 19:51:26 +0200

    **Server: Add Server GUI functionalities.

--- Team6.Server/src/ServerController.java
+++ Team6.Server/src/ServerController.java
@@ -1,44 +1,45 @@
 
+import java.net.URL;
+import java.util.ResourceBundle;
+
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.TextArea;
+import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.scene.paint.Paint;
 import javafx.scene.shape.Circle;
 
-public class ServerController 
+public class ServerController implements Initializable 
 {
-	@FXML
-	private Button btn_db_start;
-
-	@FXML
-	private Button btn_db_stop;
+	/*Database start-stop button definition*/
+	@FXML private Button btn_db_start;
+	@FXML private Button btn_db_stop;
 	
-	@FXML
-	private Circle circle_db_on;
+	/*Database status definition*/
+	@FXML private Circle circle_db_on;	
+	@FXML private Circle circle_db_off;
 	
-	@FXML
-	private Circle circle_db_off;
+	/*Connectivity start-stop button definition*/
+	@FXML private Button btn_connectivity_start;
+	@FXML private Button btn_connectivity_stop;
 	
-	@FXML
-	private Button btn_connectivity_start;
-
-	@FXML
-	private Button btn_connectivity_stop;
+	/*Connectivity status definition*/
+	@FXML private Circle circle_connectivity_on;
+	@FXML private Circle circle_connectivity_off;
 	
-	@FXML
-	private Circle circle_connectivity_on;
+	/*Log text view definition*/
+	@FXML private TextArea log;
 	
-	@FXML
-	private Circle circle_connectivity_off;
-	
-	@FXML
-	private TableView<String> setting_table;
-	
-	@FXML
-	private TextArea log;
+	/*Setting table and columns definition*/
+	@FXML private TableView<SettingsRow> setting_table;
+	@FXML private TableColumn<SettingsRow, String> setting_column;
+	@FXML private TableColumn<SettingsRow, String>  value_column;
 	
 	
 	@FXML
@@ -66,6 +67,7 @@ public class ServerController
 		btn_connectivity_stop.setDisable(false);
 		circle_connectivity_on.setFill(Paint.valueOf("green"));
 		circle_connectivity_off.setFill(Paint.valueOf("grey"));
+		setting_table.getItems().add(new SettingsRow("IPIPIP","PPPP"));
 	}
 	
 	@FXML
@@ -77,11 +79,35 @@ public class ServerController
 		circle_connectivity_off.setFill(Paint.valueOf("red"));
 	}
 	
+	
+	
 	@FXML
-	private void initialize()
+	void logChanged(ActionEvent event)
 	{
+		log.selectPositionCaret(log.getLength()); 
+        log.deselect();
+	}
+	
+	
+	ObservableList<SettingsRow> getSettings()
+	{
+		ObservableList<SettingsRow> settings = FXCollections.observableArrayList();
+		settings.add(new SettingsRow("IP","1.1.1.1"));
+		settings.add(new SettingsRow("PORT","5555"));
+		return settings;
+	}
+	
+	@Override
+	public void initialize(URL url,ResourceBundle rb)
+	{
+		setting_column.setCellValueFactory(new PropertyValueFactory<>("setting"));
+		value_column.setCellValueFactory(new PropertyValueFactory<>("value"));
+		setting_table.setItems(getSettings());
 		setting_table.setColumnResizePolicy(TableView.CONSTRAINED_RESIZE_POLICY);
+		log.setText(" hi\n yo\n hi\n hi\n yo\n yo\n hi\n yo\n hi\n yo\n hi\n yo\n hi");
 		
 		
 	}
+	
+
 }
