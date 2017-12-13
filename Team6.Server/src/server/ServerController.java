00981d0eaaaa050786c7970f661a60511cbc9100
Roman Glozman <RomanGlozman92@gmail.com>
2017-12-13 21:17:11 +0200

    ** Refactoring commit: In Server project the all the elements in default packge moved to new package: server.
    Signed-off-by: Roman Glozman <RomanGlozman92@gmail.com>

--- Team6.Server/src/server/ServerController.java
+++ Team6.Server/src/server/ServerController.java
@@ -1,3 +1,4 @@
+package server;
 
 import java.net.URL;
 import java.sql.ResultSet;
--- Team6.Server/src/server/SettingsRow.java
+++ Team6.Server/src/server/SettingsRow.java
@@ -1,3 +1,4 @@
+package server;
 import configurations.ServerConfiguration;
 import javafx.scene.control.TableView;
 
