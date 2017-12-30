672c38f2d047b08b9570b04c6f6d7be94a6b587c
Roman Glozman <RomanGlozman92@gmail.com>
2017-12-30 23:50:28 +0200

    ** Common: Entities- product entity that relevant to the prototype removed.
    ** General: The SQL query that create the schema modified to the all last changes.

--- ZerliSchemaCreate.sql
+++ ZerliSchemaCreate.sql
@@ -4,25 +4,30 @@ CREATE SCHEMA `zer-li`;
 
 USE `zer-li`;
 
-CREATE TABLE products (
-	pId INT NOT NULL,
-	pName VARCHAR(20) NOT NULL,
-	pType VARCHAR(20) NOT NULL,
-	PRIMARY KEY (pId)
+CREATE TABLE items (
+	iId INT NOT NULL AUTO_INCREMENT,
+	iName VARCHAR(20) NOT NULL,
+	iType VARCHAR(20) NOT NULL,
+	iPrice FLOAT NOT NULL,
+	iImage BLOB,
+	PRIMARY KEY (iId)
 )ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 CREATE TABLE users (
-	uName VARCHAR(10) NOT NULL,
-	uPassword VARCHAR(10) NOT NULL,
+	uUserName VARCHAR(20) NOT NULL,
+	uPassword VARCHAR(20) NOT NULL,
+	uEmail VARCHAR(40) NOT NULL,
 	uPrivilege VARCHAR(20) NOT NULL,
-	uConnected TINYINT(1) UNSIGNED NOT NULL DEFAULT 0,
-	PRIMARY KEY (uName)
+	uConnected VARCHAR(12) NOT NULL DEFAULT 'disconnected' ,
+	PRIMARY KEY (uUserName)
 )ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
-LOCK TABLES product WRITE;
+LOCK TABLES items WRITE;
 
-INSERT INTO product VALUES (1,'Rose','Flower'),(2,'Sunflower','Flower'),(3,'Lily','Flower'),(4,'Anemone','Flower'),(5,'Aconite','Flower'),(6,'Balloon Flower','Flower'),(7,'Canterbury Bells','Flower'),(8,'Dusty Miller','Flower'),(9,'Epimedium','Flower'),(10,'Fennel','Flower'),(11,'Gaillardia','Flower'),(12,'Hepatica','Flower'),(13,'Iris','Flower'),(14,'Lavender','Flower'),(15,'Marigold','Flower'),(16,'Orchid','Flower');
+INSERT INTO items (iName,iType,iPrice) VALUES ('Rose','Flower',1.0),('Sunflower','Flower',2.0),('Lily','Flower',3.0),('Anemone','Flower',4.0),('Aconite','Flower',5.0),('Balloon Flower','Flower',6.0),('Canterbury Bells','Flower',7.0),('Dusty Miller','Flower',8.0),('Epimedium','Flower',9.0),('Fennel','Flower',10.0),('Gaillardia','Flower',11.0),('Hepatica','Flower',12.0),('Iris','Flower',13.0),('Lavender','Flower',14.0),('Marigold','Flower',15.0),('Orchid','Flower',16.0);
 
-INSERT INTO users VALUES ('admin','admin','administrator',0);
+LOCK TABLES users WRITE;
+
+INSERT INTO users (uUserName,uPassword,uEmail,uPrivilege) VALUES ('companyemployee','companyemployee','companyemployee@local','companyemployee'),('shopmanager','shopmanager','shopmanager@local','shopmanager'),('chainmanager','chainmanager','chainmanager@local','chainmanager'),('administrator','administrator','administrator@local','administrator'),('shopemployee','shopemployee','shopemployee@local','shopemployee'),('costumerservice','costumerservice','costumerservice@local','costumerservice'),('costumer','costumer','costumer@local','costumer'),('servicespecialist','servicespecialist','servicespecialist@local','servicespecialist');
 
 UNLOCK TABLES;
