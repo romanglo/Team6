9fcace49b440bcebef1261bc7edf6e352d80df06
Roman Glozman <RomanGlozman92@gmail.com>
2017-12-30 23:29:01 +0200

    ** Common : Entities - created user entity.

---
+++ Team6.Common/src/entities/UserPrivilege.java
@@ -0,0 +1,21 @@
+package entities;
+
+/**
+ * 
+ *
+ * UserPrivilege:
+ * TODO Roman: Auto-generated type stub - Change with type description
+ *
+ */
+@SuppressWarnings("javadoc")
+public enum UserPrivilege {
+
+	CompanyEmployee,
+	ShopManager,
+	ChainManager,
+	Administrator,
+	ShopEmployee,
+	CostumerService,
+	Costumer,
+	ServiceSpecialist
+}
---
+++ Team6.Common/src/entities/UserStatus.java
@@ -0,0 +1,14 @@
+package entities;
+
+/**
+ * 
+ *
+ * UserStatus:
+ * TODO Roman: Auto-generated type stub - Change with type description
+ *
+ */
+@SuppressWarnings("javadoc")
+public enum UserStatus {
+
+Connected,Disconnected,Blocked
+}
