5d0025cce3b7bc101b64f3b6763bf15777267be9
Yoni Shpund <ShpundYoni@s.braude.ac.il>
2018-02-02 15:54:39 +0200

    ** Testing: Update the ClientMock with refactoring.
    Signed-off-by: Yoni Shpund <ShpundYoni@s.braude.ac.il>

--- Team6.Client/src/client/ClientMock.java
+++ Team6.Client/src/client/ClientMock.java
@@ -45,52 +45,34 @@ public class ClientMock implements IMessageSender
 	 * {@inheritDoc}
 	 */
 	@Override
+	public void setMessagesHandler(IMessageReceiveHandler messagesHandler)
+	{
+		m_messagesHandler = messagesHandler;
+	}
+
+	/**
+	 * {@inheritDoc}
+	 */
+	@Override
 	public boolean sendMessageToServer(Message data)
 	{
-		boolean isSuccess = false;
-		RespondMessageData respondMessageData;
-		EntityData newEntityData = null;
 		IMessageData messageData = data.getMessageData();
 		EntityData entityData = (EntityData) messageData;
-
 		IEntity entity = entityData.getEntity();
+
+		RespondMessageData respondMessageData;
 		if (entity instanceof Costumer) {
 			/* Received costumer entity. */
-			Costumer costumer = (Costumer) entity;
-			if (costumer.getId() != m_costumer.getId()) {
-				newEntityData = new EntityData(EntityDataOperation.None, costumer);
-				isSuccess = false;
-			} else {
-				m_costumer.setBalance(costumer.getBalance());
-				newEntityData = new EntityData(EntityDataOperation.None, m_costumer);
-				isSuccess = true;
-			}
+			respondMessageData = handleCostumerReceived(entity);
 		} else if (entity instanceof Reservation) {
 			/* Received reservation entity. */
-			Reservation reservation = (Reservation) entity;
-			Reservation toUpdate = null;
-			boolean isPresent = false;
-
-			for (Reservation reserv : m_reservationList) {
-				if (reserv.getId() == reservation.getId() && reserv.getCostumerId() == reservation.getCostumerId()
-						&& reserv.getShopManagerId() == reservation.getShopManagerId()) {
-					isPresent = true;
-					toUpdate = reserv;
-					break;
-				}
-			}
-
-			if (isPresent) {
-				toUpdate.setType(reservation.getType());
-				isSuccess = true;
-				newEntityData = new EntityData(EntityDataOperation.None, toUpdate);
-			} else {
-				isSuccess = false;
-				newEntityData = new EntityData(EntityDataOperation.None, reservation);
-			}
+			respondMessageData = handleReservationReceived(entity);
+		} else {
+			/* Entity received not of the type expected. */
+			EntityData newEntityData = new EntityData(EntityDataOperation.None, entity);
+			respondMessageData = new RespondMessageData(newEntityData, false);
 		}
 
-		respondMessageData = new RespondMessageData(newEntityData, isSuccess);
 		Message message = MessagesFactory.createIMessageDataMessage(respondMessageData);
 		try {
 			m_messagesHandler.onMessageReceived(message);
@@ -103,11 +85,64 @@ public class ClientMock implements IMessageSender
 	}
 
 	/**
-	 * {@inheritDoc}
+	 * Method that handles an entity when it is an instance of {@link Costumer}.
+	 *
+	 * @param entity
+	 *            The entity received.
+	 * @return {@link RespondMessageData} to return to the message response
+	 *         receiver.
 	 */
-	@Override
-	public void setMessagesHandler(IMessageReceiveHandler messagesHandler)
+	private RespondMessageData handleCostumerReceived(IEntity entity)
 	{
-		m_messagesHandler = messagesHandler;
+		boolean isSuccess;
+		EntityData newEntityData;
+		Costumer costumer = (Costumer) entity;
+		if (costumer.getId() != m_costumer.getId()) {
+			newEntityData = new EntityData(EntityDataOperation.None, costumer);
+			isSuccess = false;
+		} else {
+			m_costumer.setBalance(costumer.getBalance());
+			newEntityData = new EntityData(EntityDataOperation.None, m_costumer);
+			isSuccess = true;
+		}
+
+		return new RespondMessageData(newEntityData, isSuccess);
+	}
+
+	/**
+	 * Method that handles an entity when it is an instance of {@link Reservation}.
+	 *
+	 * @param entity
+	 *            The entity received.
+	 * @return {@link RespondMessageData} to return to the message response
+	 *         receiver.
+	 */
+	private RespondMessageData handleReservationReceived(IEntity entity)
+	{
+		boolean isSuccess;
+		EntityData newEntityData;
+		Reservation reservation = (Reservation) entity;
+		Reservation toUpdate = null;
+		boolean isPresent = false;
+
+		for (Reservation reserv : m_reservationList) {
+			if (reserv.getId() == reservation.getId() && reserv.getCostumerId() == reservation.getCostumerId()
+					&& reserv.getShopManagerId() == reservation.getShopManagerId()) {
+				isPresent = true;
+				toUpdate = reserv;
+				break;
+			}
+		}
+
+		if (isPresent) {
+			toUpdate.setType(reservation.getType());
+			isSuccess = true;
+			newEntityData = new EntityData(EntityDataOperation.None, toUpdate);
+		} else {
+			isSuccess = false;
+			newEntityData = new EntityData(EntityDataOperation.None, reservation);
+		}
+
+		return new RespondMessageData(newEntityData, isSuccess);
 	}
 }
